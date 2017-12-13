a70ae2c8443e1fdfa30996266bf5923257748593
kjy2143 <kjy2143@gmail.com>
2017-12-13 17:03:21 +0900

    init

---
+++ bs-config.js
@@ -0,0 +1,22 @@
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+    "server": true,
+    "files": ["html/**/*.html", "js/**/*.js"],
+    "startPath": "html/index.html",
+    "open": "local",
+    // "open": false,
+    "cors": true,
+    "reloadOnRestart": true
+};
